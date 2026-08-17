ba507329d0f63c32ebc9a01ea5f9a51812659eea
Nusaindotek <nusaindoteknologi@gmail.com>
2026-08-17 14:41:04 +0700

    Add T-Shirt size chart configuration

---
+++ src/config/sizeChart.js
@@ -0,0 +1,16 @@
+/**
+ * ApparelCAD - T-Shirt Size Chart (MVP V1.0)
+ * Satuan: Sentimeter (cm)
+ */
+export const TShirtSizeChart = {
+    M: {
+        bodyLength: 70,
+        chestWidth: 50,
+        shoulderWidth: 44,
+        neckWidth: 18,
+        armholeStraight: 24,
+        shoulderDrop: 3,
+        frontNeckDrop: 10,
+        backNeckDrop: 2.5 // Turunan leher belakang (jauh lebih dangkal)
+    }
+};

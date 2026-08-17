a866eac468aa8ba3e4439f4c76d13e519140ae72
Nusaindotek <nusaindoteknologi@gmail.com>
2026-08-17 15:20:05 +0700

    Update sizeChart.js

--- src/config/sizeChart.js
+++ src/config/sizeChart.js
@@ -1,8 +1,22 @@
 /**
- * ApparelCAD - T-Shirt Size Chart (MVP V1.0)
+ * ApparelCAD - T-Shirt Size Chart (Grading S - XL)
  * Satuan: Sentimeter (cm)
  */
 export const TShirtSizeChart = {
+    S: {
+        bodyLength: 68,
+        chestWidth: 47,
+        shoulderWidth: 42,
+        neckWidth: 17,
+        armholeStraight: 23,
+        shoulderDrop: 3,
+        frontNeckDrop: 9.5,
+        backNeckDrop: 2.5,
+        sleeveLength: 21,
+        sleeveOpening: 16,
+        sleeveCapHeight: 11.5,
+        ribWidth: 2
+    },
     M: {
         bodyLength: 70,
         chestWidth: 50,
@@ -12,10 +26,37 @@ export const TShirtSizeChart = {
         shoulderDrop: 3,
         frontNeckDrop: 10,
         backNeckDrop: 2.5,
-        // -- Parameter Lengan & Rib --
-        sleeveLength: 22,       // Panjang Lengan
-        sleeveOpening: 17,      // Lebar Bukaan Bawah Lengan (Setengah)
-        sleeveCapHeight: 12,    // Tinggi Puncak Kerungan Lengan
-        ribWidth: 2             // Lebar Jahitan Rib Leher
+        sleeveLength: 22,
+        sleeveOpening: 17,
+        sleeveCapHeight: 12,
+        ribWidth: 2
+    },
+    L: {
+        bodyLength: 72,
+        chestWidth: 53,
+        shoulderWidth: 46,
+        neckWidth: 19,
+        armholeStraight: 25,
+        shoulderDrop: 3.2,
+        frontNeckDrop: 10.5,
+        backNeckDrop: 2.5,
+        sleeveLength: 23,
+        sleeveOpening: 18,
+        sleeveCapHeight: 12.5,
+        ribWidth: 2
+    },
+    XL: {
+        bodyLength: 74,
+        chestWidth: 56,
+        shoulderWidth: 48,
+        neckWidth: 20,
+        armholeStraight: 26,
+        shoulderDrop: 3.5,
+        frontNeckDrop: 11,
+        backNeckDrop: 2.5,
+        sleeveLength: 24,
+        sleeveOpening: 19,
+        sleeveCapHeight: 13,
+        ribWidth: 2
     }
 };
